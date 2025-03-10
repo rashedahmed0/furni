ae4af08bfa5587f3d9cf18c69cb91211c8010648
Rialahmed0 <rashedahommod0@gmail.com>
2025-03-11 04:15:10 +0600

    final design

--- script.js
+++
@@ -1,8 +0,0 @@
-function sendToCart(productImg, productTitle, ProductPrice) {
-    let title = document.getElementById(productTitle).innerText;
-    console.log(title);
-    let price = document.getElementById(ProductPrice).innerText;
-    console.log(price);
-    let img = document.getElementById(productImg).src;
-    console.log(img);
-}
